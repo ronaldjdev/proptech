c5f907bd0f1835ecb366e588c0e750e099aabeaf
ronaldjdev <eronald2009@gmail.com>
2024-03-27 01:57:59 -0500

    Feat: Pantilla de como funciona terminada

--- dcapp/src/components/CardHowWork.tsx
+++ dcapp/src/components/CardHowWork.tsx
@@ -6,15 +6,14 @@ interface Props {
 export const CardHowWork = ({ icon, title, subtitle }: Partial<Props>) => {
   return (
     <>
-      <div className="flex items-center bg-lime-300 w-fit p-5 gap-4 rounded-2xl">
-        <div className="w-24 h-24 bg-gray-300">
-          {icon}
-        </div>
-        <div className="flex flex-col w-96 gap-4">
-          <h2 className="text-2xl font-medium">{title}</h2>
-          <p className="text-base">{subtitle}</p>
-        </div>
+      <div className="w-24 h-24 bg-gray-300">
+        {icon}
       </div>
+      <div className="flex flex-col gap-4 w-96">
+        <h2 className="text-2xl font-medium">{title}</h2>
+        <p className="text-base">{subtitle}</p>
+      </div>
+
     </>
   )
 }
--- dcapp/src/components/HowWork.tsx
+++ dcapp/src/components/HowWork.tsx
@@ -38,21 +38,13 @@ export const HowWork = () => {
   return (
     <>
       <Section>
-        <div className="flex flex-col w-full h-full">
+        <div className="flex flex-col w-full h-full ">
           <Title title="¿Cómo funciona?" subtitle="Prospekt" />
-          <div className="flex gap-4 flex-grow w-full ">
+          <div className="flex w-full justify-center gap-4 flex-wrap mt-10">
             {
               itemsHowWork.map((item, index) => (
-                <div className="w-1/2">
-                  <div className="w-1/2">
-                    <CardHowWork
-                      key={index}
-                      icon={item.icon}
-                      title={item.title}
-                      subtitle={item.subtitle}
-                    />
-                  </div>
-                  <div className="w-1/2">
+                <div className="w-[590px]">
+                  <div className="flex items-center justify-center bg-lime-300 w-full h-48 gap-4 p-4 rounded-2xl">
                     <CardHowWork
                       key={index}
                       icon={item.icon}
@@ -61,7 +53,6 @@ export const HowWork = () => {
                     />
                   </div>
                 </div>
-                
               ))
             }
           </div>

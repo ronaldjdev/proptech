6948f47c611201bd64078e4974e12b22c67a0044
ronaldjdev <eronald2009@gmail.com>
2024-04-04 01:21:18 -0500

    Fix: Imagenes optimizadas

--- dcapp/src/components/sections/Team.tsx
+++ dcapp/src/components/sections/Team.tsx
@@ -3,6 +3,7 @@ import { CardProfesionTeam } from "../cards/CardProfesionTeam"
 import { Section } from "../Section"
 import { Title } from "../Title"
 import { profesionales } from "../../libs/profesionales"
+import Image from "next/image"
 
 export const Team = () => {
   return (
@@ -19,7 +20,7 @@ export const Team = () => {
             <div className="w-1/3 h-96 rounded-2xl flex flex-col items-center gap-4">
               <h2 className="text-base font-bold first-letter:capitalize text-teal-600">Contamos con los mejores <br />
                 profesionales</h2>
-              <img src={profesionales[0].image} className="h-full w-60 rounded-2xl" />
+              <Image src={profesionales[0].image} alt="profesional" className="h-full w-60 rounded-2xl" />
             </div>
             <div className="w-1/3 h-96 rounded-2xl flex flex-col justify-end">
               <CardInfoProfesionTeam {...profesionales[0]} />

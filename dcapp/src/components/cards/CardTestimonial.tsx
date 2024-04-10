abb35a9671d199a2da844fcb21c34ec3dece3944
ronaldjdev <eronald2009@gmail.com>
2024-04-10 02:14:30 -0500

    Fix: Testimonial Responsive and feat contact responsive

--- dcapp/src/components/cards/CardTestimonial.tsx
+++ dcapp/src/components/cards/CardTestimonial.tsx
@@ -9,7 +9,7 @@ interface Props {
 
 export const CardTestimonial = ({ name, score, create, content, image }: Partial<Props>) => {
   return (
-    <div className="bg-gray-300 min-w-[450px] h-40 rounded-2xl p-4 md:overflow-hidden">
+    <div className="bg-gray-300 min-w-[390px] h-40 rounded-2xl p-4 md:overflow-hidden">
       <div className="flex justify-between mb-1">
         <div className="flex gap-1 items-center">
           <Image src={image} alt="" width={0} height={0} className="rounded-full w-10 h-10 object-cover" />

0dffe91dbf3d1caf041f95226d962f972ae5e94e
ronaldjdev <eronald2009@gmail.com>
2024-04-10 01:11:16 -0500

    Feat: welcome Responsive

--- dcapp/src/components/sections/Welcome.tsx
+++ dcapp/src/components/sections/Welcome.tsx
@@ -11,7 +11,7 @@ export const Welcome = () => {
   return (
     <>
       <Section id="projects">
-        <div className="flex">
+        <div className="flex flex-col md:flex-row">
           <div className="flex flex-col w-full">
             <Title title="Bienvenidos a" subtitle="Prospekt" />
             <div className="flex gap-4 my-10 items-end">
@@ -23,7 +23,7 @@ export const Welcome = () => {
 
               </h2>
             </div>
-            <p className="text-base text-zinc-900 mb-10">
+            <p className="text-base text-zinc-900 mb-10 text-justify ">
               <strong>Prospekt</strong> ofrece una forma innovadora y colaborativa de aprovechar
               el potencial de los terrenos vacíos, beneficiando a propietarios,
               inversores y comunidades por igual.
@@ -32,10 +32,10 @@ export const Welcome = () => {
 
               ¡Únete a nosotros para transformar terrenos en oportunidades!
             </p>
-            <Button white text="Unirse" icon="👋" />
+            <Button dark text="Unirse" icon="👋" size="w-full md:w-fit" />
           </div>
-          <div className="flex gap-4 w-full justify-end">
-            <div className="w-52 h-40 mt-10 bg-lime-300 rounded-2xl px-3 py-6 flex gap-4">
+          <div className="flex flex-col md:flex-row gap-4 w-full justify-end">
+            <div className="w-full md:w-52 h-40 mt-10 bg-lime-300 rounded-2xl px-3 py-6 flex gap-4">
               <Image
                 className="w-14 h-14 bg-gray-300 remove-bg"
                 src={success}
@@ -44,13 +44,13 @@ export const Welcome = () => {
                 height={0}
                 unoptimized
               />
-              <h2 className="w-1/2 text-xl text-teal-600">
+              <h2 className="md:w-1/2 text-xl text-teal-600">
                 <span className="text-6xl">10K</span>
                 <br />
                 Propietarios felices
               </h2>
             </div>
-            <Image src={img4} alt="edificio" width={0} height={0} className="w-1/2 h-full bg-lime-300 rounded-2xl object-cover" />
+            <Image src={img4} alt="edificio" width={0} height={0} className="w-full md:w-1/2 h-full bg-lime-300 rounded-2xl object-cover" />
           </div>
         </div>
       </Section>
